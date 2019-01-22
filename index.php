898faa493c4606c36daaf52182f41dd2ea68c34f
Evgenii Ryshkov <eryshkov@gmail.com>
2019-01-23 00:37:09 +0300

    bin fixed

--- index.php
+++ index.php
@@ -5,11 +5,26 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test</title>
+    <title>Home Work</title>
 </head>
 <body>
-<?php
-echo 'test';
-?>
+    <h1>
+        <?php
+        function toBin($string)
+        {
+            $arr = str_split($string);
+            $result = [];
+            foreach ($arr as $letter) {
+                $result[] = decbin(ord($letter));
+            }
+            return implode(' ', $result);
+        }
+
+        echo toBin('٣');
+        echo '<br>';
+        echo toBin('фв');
+
+        ?>
+    </h1>
 </body>
 </html>
