36a25f76040b4449366628f5165f88a6bc2ffcad
Evgenii Ryshkov <eryshkov@gmail.com>
2019-01-26 21:35:59 +0300

    homework 2

--- hw02/index.php
+++ hw02/index.php
@@ -7,12 +7,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
-    <title>Menu</title>
+    <title>HomeWork 2</title>
 </head>
 <body>
-        <p></p>
-        <div class="container">
-            <a href="/hw02" class="btn btn-primary">HomeWork 2</a>
-        </div>
+    <p></p>
+    <div class="container">
+        <a href="/hw02/01" class="btn btn-primary">Task 1</a>
+    </div>
 </body>
 </html>
