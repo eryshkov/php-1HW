ec65a512c7f7d578ff6d74b30f861759006a602e
Evgenii Ryshkov <eryshkov@gmail.com>
2019-01-31 17:40:08 +0300

    second task if fixed

--- 02/index.php
+++ 02/index.php
@@ -4,46 +4,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+            crossorigin="anonymous"></script>
 
     <title>Task 2</title>
 </head>
 <body>
-    <p></p>
-    <div class="container">
-        <h1>Task 2</h1>
-        <?php
-        function discriminant($a, $b, $c) {
-            return $b * $b - 4 * $a * $c;
-        }
+<p></p>
+<div class="container">
+    <h1>Task 2</h1>
+    <?php
+    function discriminant($a, $b, $c)
+    {
+        return $b * $b - 4 * $a * $c;
+    }
 
-        assert(16 === discriminant(1, -8, 12));
-        assert(0 === discriminant(1, -6, 9));
-        assert(-131 === discriminant(5, 3, 7));
+    assert(16 === discriminant(1, -8, 12));
+    assert(0 === discriminant(1, -6, 9));
+    assert(-131 === discriminant(5, 3, 7));
 
-        $a = 1;
-        $b = -8;
-        $c = 12;
-        $d = discriminant($a, $b, $c);
+    $a = 1;
+    $b = -8;
+    $c = 12;
+    $d = discriminant($a, $b, $c);
 
-        echo $a . ($b < 0 ? '*x*x ': '*x*x +') . $b . ($c < 0 ? '*x ' : '*x + ') . $c . ' = 0<br>';
-        switch (true) {
-            case $d > 0:
-                $x1 = (-$b + sqrt($d)) / (2 * $a);
-                $x2 = (-$b - sqrt($d)) / (2 * $a);
-                echo 'Корни уравнения:<br>';
-                echo 'x1 = ' . $x1 . '<br>';
-                echo 'x2 = ' . $x2 . '<br>';
-                break;
-            case $d == 0:
-                $x = (-$b) / (2 * $a);
-                echo 'Корень уравнения:<br>' . 'x = ' . $x . '<br>';
-                break;
-            default:
-                echo 'Уравнение не имеет действительных корней<br>';
-        }
-        ?>
-    </div>
+    echo $a . ($b < 0 ? '*x*x ' : '*x*x +') . $b . ($c < 0 ? '*x ' : '*x + ') . $c;?> = 0<br><?php
+    switch (true) {
+        case $d > 0:
+            $x1 = (-$b + sqrt($d)) / (2 * $a);
+            $x2 = (-$b - sqrt($d)) / (2 * $a);
+            ?>Корни уравнения:<br>
+            x1 = <?php echo $x1; ?><br>
+            x2 = <?php echo $x2; ?><br><?php
+            break;
+        case $d == 0:
+            $x = (-$b) / (2 * $a);
+            ?>Корень уравнения:<br>x = <?php echo $x; ?><br><?php
+            break;
+        default:
+            ?>Уравнение не имеет действительных корней<br><?php
+    }
+    ?>
+</div>
 </body>
 </html>
